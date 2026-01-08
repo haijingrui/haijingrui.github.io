bb0c86a57947ad4c5b53574618a59525279ac23e
haijingrui <13503855846@163.com>
2026-01-08 11:08:28 +0800

    Site updated: 2026-01-08 11:08:27

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/df31.html","posts/ec81.html","posts/d7be.html","posts/78ed.html","posts/fb4c.html","posts/ab5b.html"];function toRandomPost(){
+var posts=["posts/df31.html","posts/ec81.html","posts/d7be.html","posts/ab5b.html","posts/78ed.html","posts/fb4c.html","posts/970f.html","posts/337a.html","posts/ba7e.html","posts/2d63.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
