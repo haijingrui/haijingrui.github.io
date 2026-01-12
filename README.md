8a79c20b11339b7af8afcd311b1df0b03914c558
haijingrui <13503855846@163.com>
2026-01-12 10:15:06 +0800

    Site updated: 2026-01-12 10:15:04

---
+++ README.md
@@ -0,0 +1,3 @@
+<https://haijingrui.github.io>
+
+关于Hexo的主题设置与部署，可搜索上面的链接查看我的网站
---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["posts/df31.html","posts/ec81.html","posts/d7be.html","posts/337a.html","posts/2d63.html","posts/970f.html","posts/78ed.html","posts/ba7e.html","posts/ab5b.html","posts/fb4c.html"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
