88df9cea4216122e5c827174445b7b02cf1eccea
haijingrui <13503855846@163.com>
2026-01-13 09:46:21 +0800

    Site updated: 2026-01-13 09:46:19

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/df31.html","posts/ec81.html","posts/d7be.html","posts/2d63.html","posts/337a.html","posts/78ed.html","posts/970f.html","posts/ba7e.html","posts/fb4c.html","posts/ab5b.html"];function toRandomPost(){
+var posts=["posts/df31.html","posts/ec81.html","posts/d7be.html","posts/2d63.html","posts/970f.html","posts/78ed.html","posts/ab5b.html","posts/337a.html","posts/ba7e.html","posts/fb4c.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
